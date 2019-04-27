4e3b65d92a905fd9c302f65401e1cbaceccfaaa5
Ayush Goyal <ayush987goyal@gmail.com>
2019-04-27 11:08:31 +0530

    Add icons and events handlers for the same

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Editor } from 'slate-react';
 import { Value } from 'slate';
 
-import BoldMark from './BoldMark';
-import ItalicMark from './ItalicMark';
+import Icon from 'react-icons-kit';
+import { bold } from 'react-icons-kit/feather/bold';
+import { italic } from 'react-icons-kit/feather/italic';
+
+import { BoldMark, ItalicMark, FormatToolbar } from './index';
 
 const initialValue = Value.fromJSON({
   document: {
@@ -28,6 +31,7 @@ const initialValue = Value.fromJSON({
 
 const TextEditor = () => {
   const [value, setValue] = useState(initialValue);
+  const editor = useRef();
 
   const onChange = ({ value: newValue }) => {
     setValue(newValue);
@@ -66,7 +70,32 @@ const TextEditor = () => {
     }
   };
 
-  return <Editor value={value} onChange={onChange} onKeyDown={onKeyDown} renderMark={renderMark} />;
+  const onMarkClick = (e, type) => {
+    e.preventDefault();
+
+    editor.current.toggleMark(type);
+  };
+
+  return (
+    <>
+      <FormatToolbar>
+        <button className="tooltip-icon-button" onPointerDown={e => onMarkClick(e, 'bold')}>
+          <Icon icon={bold} />
+        </button>
+        <button className="tooltip-icon-button" onPointerDown={e => onMarkClick(e, 'italic')}>
+          <Icon icon={italic} />
+        </button>
+      </FormatToolbar>
+
+      <Editor
+        ref={editor}
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        renderMark={renderMark}
+      />
+    </>
+  );
 };
 
 export default TextEditor;
