3f593a1fe30132a82c3a18877c82c85d9b3fa89f
Ayush Goyal <ayush987goyal@gmail.com>
2019-04-27 09:30:58 +0530

    Add bold and italic marks

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Editor } from 'slate-react';
 import { Value } from 'slate';
 
+import BoldMark from './BoldMark';
+import ItalicMark from './ItalicMark';
+
 const initialValue = Value.fromJSON({
   document: {
     nodes: [
@@ -30,7 +33,40 @@ const TextEditor = () => {
     setValue(newValue);
   };
 
-  return <Editor value={value} onChange={onChange} />;
+  const onKeyDown = (e, change) => {
+    if (!e.ctrlKey) return;
+    e.preventDefault();
+
+    switch (e.key) {
+      case 'b': {
+        change.toggleMark('bold');
+        return true;
+      }
+
+      case 'i': {
+        change.toggleMark('italic');
+        return true;
+      }
+
+      default:
+        return true;
+    }
+  };
+
+  const renderMark = props => {
+    switch (props.mark.type) {
+      case 'bold':
+        return <BoldMark {...props} />;
+
+      case 'italic':
+        return <ItalicMark {...props} />;
+
+      default:
+        return null;
+    }
+  };
+
+  return <Editor value={value} onChange={onChange} onKeyDown={onKeyDown} renderMark={renderMark} />;
 };
 
 export default TextEditor;
